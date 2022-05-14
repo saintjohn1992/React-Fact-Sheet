a3f10fd342417a9695d8d883a677620475963d8a
saintjohn1992 <saintjohnshane@outlook.com>
2022-05-14 18:59:17 +0200

    first react project

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+
+function Main() {
+    return(
+        <div className="container">
+            <h1 className="title">Fun facts about React</h1>
+
+            <ul className="list-group">
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>React is maintained by Facebook.</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
+                <li>React is not imperative, but insted declarative.</li>
+            </ul>
+        </div>
+        )
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+    return (
+        <nav>
+            <img src="/logo192.png" className="nav-logo" alt="logo" />
+            <h3 className="header">ReactFacts</h3>
+            <h4 className="subheader">React project #1</h4>
+        </nav>
+    )
+}
+
+export default Navbar;
